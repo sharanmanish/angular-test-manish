bfd5f24bb8d22acad140326dceb7d2017e1c2209
dev-sharan <sharan.manish@ymail.com>
2019-07-13 21:11:23 +0530

    main ui added

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'List Title',
+    children: [
+      {name: 'Apple'},
+      {name: 'Banana'},
+      {name: 'Fruit loops'},
+      {name: 'Add New'},
+    ]
+  }
+];
 
 @Component({
   selector: 'app-main',
@@ -7,7 +26,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  panelOpenState = true;
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
 
   ngOnInit() {
   }
